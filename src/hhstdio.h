ca0ea9a0c5f09b54ffc0488646a35cada259d052
frobiac <frobiac@gmail.com>
2012-07-06 21:25:43 +0200

    removed duplicate license header

--- src/hhstdio.h
+++ src/hhstdio.h
@@ -37,30 +37,6 @@ char stdout_popchar(void);
 
 static const USB_KeyboardReport_Data_t ascii_table[];
 
-
-/*
-                    The HumbleHacker Keyboard Project
-                 Copyright © 2008-2010, David Whetstone
-               david DOT whetstone AT humblehacker DOT com
-
-  This file is a part of The HumbleHacker Keyboard Project.
-
-  The HumbleHacker Keyboard Project is free software: you can redistribute
-  it and/or modify it under the terms of the GNU General Public License as
-  published by the Free Software Foundation, either version 3 of the
-  License, or (at your option) any later version.
-
-  The HumbleHacker Keyboard Project is distributed in the hope that it will
-  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-  Public License for more details.
-
-  You should have received a copy of the GNU General Public License along
-  with The HumbleHacker Keyboard Project.  If not, see
-  <http://www.gnu.org/licenses/>.
-
-*/
-
 /*
    The largest HID usage code is AC_Distribute_Vertically from the Consumer
    Control usage page.  Its id is 0x29c, which requires 10 bits.  The largest
