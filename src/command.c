f2eeca4d2490cb86b9db8f3680cda85185a108f1
frobiac <frobiac@gmail.com>
2013-02-06 13:03:05 +0100

    Copyright updated to 2013
    @todo : check remaining files

--- src/command.c
+++ src/command.c
@@ -1,5 +1,5 @@
 /*
-    Copyright 2010-2012 Stefan Fröbe, <frobiac /at/ gmail [d0t] com>
+    Copyright 2010-2013 Stefan Fröbe, <frobiac /at/ gmail [d0t] com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
