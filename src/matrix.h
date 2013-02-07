19e3990f2d78ffaa9459df8dde42b542a81bce40
frobiac <frobiac@gmail.com>
2013-02-07 23:11:53 +0100

    Teensy-2.0++ not used in any keyboard anymore.

--- src/matrix.h
+++ src/matrix.h
@@ -19,35 +19,31 @@
 #ifndef MATRIX_H
 #define  MATRIX_H
 
+/** @file matrix.h
+ * 
+ *  This is a description of the hardware setup used in the BlueCube.
+ *  Uses Teensy-2.0 .
+ *  
+ */
 
 // this must be called once before matrix_scan.
 static inline uint8_t read_col(void)
 {
-#if defined(__AVR_ATmega32U4__)
     uint8_t res = PINF;
     // flipped teensy 2.0: F 01  4567
     return (( res & 0b11) | ((res & 0b11110000)>>2 ));
-#elif defined(__AVR_AT90USB1286__)             // Teensy++ 2.0
-    // teensy++ 2.0 n // PINF & 0x11
-    return (PINF & ~((1<<6) | (1<<7)));
-#endif
 }
 
 static inline void unselect_rows(void)
 {
-#if defined(__AVR_ATmega32U4__)
     DDRD  &= 0b00001011;
     PORTD &= 0b00001011;
     DDRB  &= 0b10001111;
     PORTB &= 0b10001111;
-#elif defined(__AVR_AT90USB1286__)             // Teensy++ 2.0
-#error "PINCONFIG for unselect_rows NOT defined for __AVR_AT90USB1286__ setup"
-#endif
 }
 
 static inline void activate(uint8_t row)
 {
-#if defined(__AVR_ATmega32U4__)
     unselect_rows();
     // swap upper and lower ports to have left half first in matrix
     (row<4) ? (row+=4) : (row-=4);
@@ -63,29 +59,14 @@ static inline void activate(uint8_t row)
         case 6: DDRD |= (1<<2); break;
         case 7: DDRD |= (1<<5); break;
     }
-    return;
-
-#elif defined(__AVR_AT90USB1286__)             // Teensy++ 2.0
-#error "PINCONFIG for activate(row) NOT defined for __AVR_AT90USB1286__ setup"
-#endif
 }
 
-/// @todo Why static inline only here ?
 static inline void init_cols(void)
 {
-#if defined(__AVR_ATmega32U4__)
     // teensy 2.0: 2&3 unused, F 01  4567
     /* Columns are inputs */
     DDRF  &= ((1<<2) | (1<<3)); // 192 or upper 2 bits
     /* Enable pull-up resistors on inputs */
     PORTF |= (0b11110011);
-
-#elif defined(__AVR_AT90USB1286__)
-    // teensy++ 2.0 : 6&7 unused
-    DDRF &= ((1<<6) | (1<<7));
-    PORTF |= (0b00111111);
-    return;
-#endif
-
 }
 #endif
