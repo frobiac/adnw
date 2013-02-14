06c05eac244e3eb265bd0e9214cf5337c2451d0e
frobiac <frobiac@gmail.com>
2013-02-14 20:09:08 +0100

    use TARGET from makefile to flash correct hex file

--- build_teensy.sh
+++ build_teensy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-MCU=$( grep "^MCU *=" makefile | sed "s/.*= *//")
+MCU=$( grep "^MCU\s*=" makefile | sed "s/.*=\s*//")
+HEX=$( grep "^TARGET\s*=" makefile | sed "s/.*=\s*//")
 #MCU=atmega32u4
 #MCU=at90usb1286
+
+mv $HEX.hex $HEX.hex.old 2>/dev/null
 
 if [ ! -z $1 ]; then 
 	echo "*** make clean" 
@@ -10,8 +13,11 @@ if [ ! -z $1 ]; then
 fi
 echo "*** make " &&
 make >> log &&
-echo "*** Now flashing $MCU..." &&
 
-teensy_loader_cli -mmcu=$MCU -w -v adnw.hex &&
-
-sudo -S  /usr/sbin/hid_listen
+if [ -f $HEX.hex ]; then 
+  echo "*** Now flashing $MCU with $HEX.hex ..." &
+  teensy_loader_cli -mmcu=$MCU -w -v $HEX.hex &&
+  sudo -S  /usr/sbin/hid_listen
+else
+  echo "$HEX not found, cannot flash."
+fi
