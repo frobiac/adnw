5a93ebaf9ebccd00cdf161e234cd6ffc0759c971
frobiac <frobiac@gmail.com>
2014-10-07 12:48:57 +0200

    DEBUG Cleanup obsolete keymap comments

--- src/keymap.h
+++ src/keymap.h
@@ -124,16 +124,6 @@ RedTilt: outermost "thumb" keys are in topmost row, and ring finger one is missi
 }
 #endif
 
-/* Space/Shift in center
-  { t36,t32,no30,t35,t33,t34 }, \
-  ...
-  { t72,t73,t71,no76,t70,t74 } \
-
- * Space/Shift 2nd from middle:
-  { t36,t32,no30,t33,t34,t35 }, \
-  ...
-  { t71,t72,t73,no76,t70,t74 } \
-*/
 
 #ifdef HYPERNANO
   #define CMD_MODE() ( (rowData[3] & (1<<1)) && (rowData[7] & (1<<5)) ) 
@@ -163,7 +153,6 @@ RedTilt: outermost "thumb" keys are in topmost row, and ring finger one is missi
   no00,k01,k02,k03,k04,k05,        k40,k41,k42,k43,k44,no45, \
   no10,k11,k12,k13,k14,k15,        k50,k51,k52,k53,k54,no55, \
   no20,k21,k22,k23,k24,k25,        k60,k61,k62,k63,k64,no65, \
-/*                              M3 M2  SHF  M1  AGr A  B  */ \
    t30,t31,ALT,CTL,SPC,GUI,MAC,  M3,M2,SHF,M1,AGR,t74,t75   \
 ) { \
   {  no, k01,k02,k03,k04,k05 }, \
